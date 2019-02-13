aaf5144ba7a328941e4b5f8fe227b5665b24682a
Victor Kobayashi <victork@snaptravel.com>
2019-02-13 17:23:48 -0500

    changed description variable for yahoo

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -54,12 +54,12 @@ export default class helpers {
 
       case "yahoo":
         // yahoo doesn't utilize endTime so we need to calulate duration
-        let description = event.description.replace(/^[\s\t]+/,'')
+        let formattedDescription = event.description.replace(/^[\s\t]+/,'')
         calendarUrl = "https://calendar.yahoo.com/?v=60&view=d&type=20";
         calendarUrl += "&title=" + encodeURIComponent(event.title);
         calendarUrl += "&st=" + this.formatStartTime(event.startTime);
         calendarUrl += "&et=" + this.formatEndTime(event.endTime);
-        calendarUrl += "&desc=" + encodeURIComponent(event.description);
+        calendarUrl += "&desc=" + encodeURIComponent(formattedDescription);
         calendarUrl += "&in_loc=" + encodeURIComponent(event.location);
         break;
 
